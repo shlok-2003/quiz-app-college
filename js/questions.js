2530afc76e25536b23fd6106e6b35157bc63b3a4
shlok-2003 <shlokprajapati2003@gmail.com>
2024-02-15 16:54:17 +0530

    Made changes in questons

--- js/questions.js
+++ js/questions.js
@@ -1,57 +1,123 @@
-let questions = [
+const indianHistoryQuestions = [
     {
-    numb: 1,
-    question: "What does HTML stand for?",
-    answer: "Hyper Text Markup Language",
-    options: [
-      "Hyper Text Preprocessor",
-      "Hyper Text Markup Language",
-      "Hyper Text Multiple Language",
-      "Hyper Tool Multi Language"
-    ]
-  },
+        numb: 6,
+        question:
+            'Who was the first Emperor of the Maurya Dynasty in ancient India?',
+        answer: 'Chandragupta Maurya',
+        options: [
+            'Ashoka the Great',
+            'Chandragupta Maurya',
+            'Bindusara',
+            'Chanakya',
+        ],
+    },
     {
-    numb: 2,
-    question: "What does CSS stand for?",
-    answer: "Cascading Style Sheet",
-    options: [
-      "Common Style Sheet",
-      "Colorful Style Sheet",
-      "Computer Style Sheet",
-      "Cascading Style Sheet"
-    ]
-  },
+        numb: 7,
+        question:
+            'The Indus Valley Civilization was primarily located in which modern-day country?',
+        answer: 'Pakistan',
+        options: ['India', 'Pakistan', 'Bangladesh', 'Nepal'],
+    },
     {
-    numb: 3,
-    question: "What does PHP stand for?",
-    answer: "Hypertext Preprocessor",
-    options: [
-      "Hypertext Preprocessor",
-      "Hypertext Programming",
-      "Hypertext Preprogramming",
-      "Hometext Preprocessor"
-    ]
-  },
+        numb: 8,
+        question: 'Who was the first woman Prime Minister of India?',
+        answer: 'Indira Gandhi',
+        options: [
+            'Sonia Gandhi',
+            'Indira Gandhi',
+            'Pratibha Patil',
+            'Mamata Banerjee',
+        ],
+    },
     {
-    numb: 4,
-    question: "What does SQL stand for?",
-    answer: "Structured Query Language",
-    options: [
-      "Stylish Question Language",
-      "Stylesheet Query Language",
-      "Statement Question Language",
-      "Structured Query Language"
-    ]
-  },
+        numb: 9,
+        question: 'The Battle of Plassey took place in which year?',
+        answer: '1757',
+        options: ['1764', '1757', '1857', '1675'],
+    },
     {
-    numb: 5,
-    question: "What does XML stand for?",
-    answer: "eXtensible Markup Language",
-    options: [
-      "eXtensible Markup Language",
-      "eXecutable Multiple Language",
-      "eXTra Multi-Program Language",
-      "eXamine Multiple Language"
-    ]
-  },
+        numb: 10,
+        question: 'Who was the founder of the Gupta Empire?',
+        answer: 'Sri Gupta',
+        options: ['Chandragupta Maurya', 'Sri Gupta', 'Samudragupta', 'Ashoka'],
+    },
+    {
+        numb: 11,
+        question: 'The Quit India Movement was launched in which year?',
+        answer: '1942',
+        options: ['1930', '1942', '1922', '1956'],
+    },
+    {
+        numb: 12,
+        question:
+            "The famous 'Dandi March' led by Mahatma Gandhi was a protest against?",
+        answer: 'Salt Tax',
+        options: ['Indigo Tax', 'Salt Tax', 'Land Tax', 'Income Tax'],
+    },
+    {
+        numb: 13,
+        question: 'Who was the first Mughal emperor of India?',
+        answer: 'Babur',
+        options: ['Akbar', 'Babur', 'Humayun', 'Jahangir'],
+    },
+    {
+        numb: 14,
+        question: 'The Red Fort in Delhi was built by which Mughal Emperor?',
+        answer: 'Shah Jahan',
+        options: ['Akbar', 'Aurangzeb', 'Shah Jahan', 'Jahangir'],
+    },
+    {
+        numb: 15,
+        question: 'The Indian National Congress was founded in which year?',
+        answer: '1885',
+        options: ['1857', '1901', '1885', '1920'],
+    },
+    {
+        numb: 16,
+        question: 'Who was the first Sikh Guru?',
+        answer: 'Guru Nanak',
+        options: [
+            'Guru Nanak',
+            'Guru Gobind Singh',
+            'Guru Angad',
+            'Guru Tegh Bahadur',
+        ],
+    },
+    {
+        numb: 17,
+        question:
+            'The ancient university of Nalanda was located in which present-day Indian state?',
+        answer: 'Bihar',
+        options: ['Uttar Pradesh', 'Bihar', 'Madhya Pradesh', 'Rajasthan'],
+    },
+    {
+        numb: 18,
+        question:
+            'Who was the famous queen of the Maratha-ruled state of Jhansi?',
+        answer: 'Rani Lakshmibai',
+        options: [
+            'Rani Padmini',
+            'Rani Lakshmibai',
+            'Rani Durgavati',
+            'Rani Ahilyabai Holkar',
+        ],
+    },
+    {
+        numb: 19,
+        question:
+            'The first war of Indian independence, also known as the Sepoy Mutiny, occurred in which year?',
+        answer: '1857',
+        options: ['1757', '1857', '1947', '1601'],
+    },
+    {
+        numb: 20,
+        question: "Who was the architect of the 'Constitution of India'?",
+        answer: 'B.R. Ambedkar',
+        options: [
+            'Jawaharlal Nehru',
+            'Sardar Patel',
+            'B.R. Ambedkar',
+            'Rajendra Prasad',
+        ],
+    },
 ];
